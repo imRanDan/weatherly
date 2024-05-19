f110fbba009b96492eb6064e0fee1e723384c7ab
Danyal Imran <dan.imran97@gmail.com>
2024-05-19 00:35:48 -0400

    working on jsx layout + added in api key through env

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=${process.env.WEATHER_API_KEY}`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Toronto</p>
+          </div>
+          <div className="temp">
+            <h1>21°C</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p>21°C</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>15km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
